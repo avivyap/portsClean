149faaca9165403dc2e32e132a4bc69612faa4d5
aviv <141031195+avivyap@users.noreply.github.com>
2025-11-21 08:26:44 +0000

    Update portsClean.sh

--- portsClean.sh
+++ portsClean.sh
@@ -22,49 +22,52 @@ trap ctrl_c INT
 
 function ctrl_c(){
 
-	echo -e "${redColour}[!] Saliendo.......${endColour}"
-	exit 1
-	tput cnorm
+        echo -e "${redColour}[!] Saliendo.......${endColour}"
+        exit 1
+        tput cnorm
 
 }
 
 function helpPanel(){
 
-	echo -e "\n${yellowColour}[+]${endColour}${grayColour} Uso: ./portsClean.sh${endColour}\n"
-	echo -e "\t${purpleColour}f)${endColour}${blueColour} Especifica el archivo del que quieres extraer los puertos${endColour}\n"
-	echo -e "\t${grayColour} Ejemplo: ./portsClean.sh -f puertos${endColour}"
-	tput cnorm
+        echo -e "\n${yellowColour}[+]${endColour}${grayColour} Uso: ./portsClean.sh${endColour}\n"
+        echo -e "\t${purpleColour}f)${endColour}${blueColour} Especifica el archivo del que quieres extraer los puertos${endColour}\n"
+        echo -e "\t${grayColour} Ejemplo: ./portsClean.sh -f puertos${endColour}"
+        tput cnorm
 }
 
 function portClean(){
 
-	echo -e "\n${whiteColour}[*]${endColour}${blueColour} Extrayendo informacion..........${endColour}\n"
+        echo -e "\n${whiteColour}[*]${endColour}${blueColour} Extrayendo informacion..........${endColour}\n"
 
-	ip=$(cat $file| awk '{print $2}' | xargs | awk '{print $3}')
-	echo -e "\t${whiteColour}[*]${endColour}${blueColour} Direccion ip:${endColour} ${purpleColour}$ip${endColour}\n"
+        ip=$(cat $file| awk '{print $2}' | xargs | awk '{print $3}')
+        echo -e "\t${whiteColour}[*]${endColour}${blueColour} Direccion ip:${endColour} ${purpleColour}$ip${endColour}\n"
 
-	echo -e "\t${whiteColour}[*]${endColour}${blueColour} Puertos:${endColour}\n"
-	for linea in $(grep "Ports:" $file | xargs | tr ' ' '\n');do
-	if [[ $linea =~ open ]];then
-		linea_2=$(echo "$linea" | tr '/' '\n' | xargs)
-		puerto=$(echo "$linea_2" | awk '{print $1}')
-		servicio=$(echo "$linea_2" | awk '{print $4}')
-		echo -e "\t\t${whiteColour}[*]${endColour}${turquoiseColour} $puerto${endColour} ${whiteColour}->${endColour} ${purpleColour}$servicio${endColour}\n"
-	fi
-	done
-	tput cnorm
+        echo -e "\t${whiteColour}[*]${endColour}${blueColour} Puertos:${endColour}\n"
+        for linea in $(grep "Ports:" $file | xargs | tr ' ' '\n');do
+        if [[ $linea =~ open ]];then
+                linea_2=$(echo "$linea" | tr '/' '\n' | xargs)
+                puerto=$(echo "$linea_2" | awk '{print $1}')
+                servicio=$(echo "$linea_2" | awk '{print $4}')
+                echo -e "\t\t${whiteColour}[*]${endColour}${turquoiseColour} $puerto${endColour} ${whiteColour}->${endColour} ${purpleColour}$servicio${endColour}\n"
+                puertos+="$puerto,"
+    fi
+        done
+        puertos=$(echo $puertos | sed 's/,$//')
+        echo -e "\n${whiteColour}[+]${endColour}${blueColour} Puertos copiados en la clipboard${endColour}\n"
+        echo -n "$puertos" | xclip -selection clipboard
+        tput cnorm
 }
 
 tput civis
 declare -i parametro=0;while getopts ":f:" arg;do
 
-	case $arg in
-		f) file=$OPTARG; let parametro=1;;
-	esac
+        case $arg in
+                f) file=$OPTARG; let parametro=1;;
+        esac
 done
 if [ $parametro == "0" ];then
-	helpPanel
+        helpPanel
 else
-	portClean
+        portClean
 fi
-
